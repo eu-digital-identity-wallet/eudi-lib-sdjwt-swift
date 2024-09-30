2ab87c627b13bb9237cd08af2c0077ec4f571463
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2024-09-30 16:58:09 +0300

    [fix] metadata fetcher

---
+++ Sources/Fetchers/SdJwtVcIssuerMetaDataFetcher.swift
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Foundation
+import SwiftyJSON
+import JSONWebKey
+
+public protocol SdJwtVcIssuerMetaDataFetching {
+  var urlSession: URLSession { get }
+  func fetchIssuerMetaData(issuer: URL) async throws -> SdJwtVcIssuerMetaData?
+}
+
+public class SdJwtVcIssuerMetaDataFetcher: SdJwtVcIssuerMetaDataFetching {
+  
+  public let urlSession: URLSession
+  
+  public init(urlSession: URLSession) {
+    self.urlSession = urlSession
+  }
+  
+  public func fetchIssuerMetaData(issuer: URL) async throws -> SdJwtVcIssuerMetaData? {
+    let issuerMetadataUrl = issuerMetadataUrl(for: issuer)
+    let metadata: SdJwtVcIssuerMetadataTO = try await fetch(from: issuerMetadataUrl)
+    
+    guard issuer == URL(string: metadata.issuer) else {
+      throw SDJWTVerifierError.invalidJwt
+    }
+    
+    try xorValues(metadata.jwksUri, metadata.jwks)
+    
+    if let jwks = metadata.jwks {
+      return .init(
+        issuer: issuer,
+        jwks: jwks.keys
+      )
+    } else if metadata.jwksUri != nil {
+      let jwks: JWKSet = try await fetch(from: issuerMetadataUrl)
+      return .init(
+        issuer: issuer,
+        jwks: jwks.keys
+      )
+    }
+    
+    return nil
+  }
+}
+
+private extension SdJwtVcIssuerMetaDataFetcher {
+  private func issuerMetadataUrl(for issuer: URL) -> URL {
+    var components = URLComponents(url: issuer, resolvingAgainstBaseURL: false)!
+    components.path = "/.well-known/jwt-vc-issuer" + components.path
+    return components.url!
+  }
+  
+  func fetch<T: Decodable>(from url: URL) async throws -> T {
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+      throw URLError(.badServerResponse)
+    }
+    
+    // Decode the JSON data into the Codable struct
+    let decoder = JSONDecoder()
+    let metadata = try decoder.decode(T.self, from: data)
+    
+    return metadata
+  }
+  
+  func xorValues(_ first: Any?, _ second: Any?) throws {
+    // Ensure that one is non-nil and the other is nil, but not both non-nil or both nil
+    guard (first != nil) != (second != nil) else {
+      throw SDJWTVerifierError.invalidJwt
+    }
+  }
+}
